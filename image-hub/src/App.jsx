f057394395c97a840e5a374e771b935a2fec01cd
shivani.rajput <shivani.rajput@officebeacon.com>
2026-02-11 18:56:34 +0530

    api calling with axiox ans pagination is working

--- image-hub/src/App.jsx
+++ image-hub/src/App.jsx
@@ -1,47 +1,64 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
+  const [index, setIndex] = useState(1);
 
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=2&limit=20",
+      `https://picsum.photos/v2/list?page=${index}&limit=20`,
     );
     setUserData(response.data);
     console.log(response.data);
   };
 
-  let printUserData = "No User available";
+  useEffect(() => {
+    getData();
+  }, [index]);
+
+  let printUserData = (
+    <h3 className="text-gray-200 text-4xl absolute top-1/2 left-1/2 -translatex-1/2  -translatey-1/2">Loading...........</h3>
+  );
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
       return (
-        <div className="ml-4" key={idx}>
-          <a href={elem.url} target="_black">
-            <div className="h-40 w-44 overflow-hidden bg-white rounded-xl">
-              <img
-                className="h-full w-full object-cover"
-                src={elem.download_url}
-                alt=""
-              />
-            </div>
-            <h2 className="font-bold text-lg">{elem.author}</h2>
-          </a>
+        <div className="m-4 " key={idx}>
+          <Card  elem={elem}/>
         </div>
       );
     });
   }
 
   return (
-    <div className="bg-black h-screen overflow-auto text-white">
-      <button
-        onClick={getData}
-        className="bg-green-500 mb-4 mt-3 ml-4 active:scale-95 cursor-pointer text-white px-5 py-2 rounded"
-      >
-        Get data
-      </button>
-
-      <div className="flex flex-wrap gap-4">{printUserData}</div>
+    <div className="bg-black h-screen overflow-auto  text-white">
+      <div className="flex flex-wrap gap-4 h-[82%]">{printUserData}</div>
+      <div className="flex justify-center gap-4 items-center p-4 ">
+        <button
+          onClick={() => {
+            console.log("Previous button click");
+            if (index > 1) {
+              setIndex(index - 1);
+              setUserData([]);
+            }
+          }}
+          className="bg-amber-300 text-black rounded px-4 cursor-pointer active:scale-95 py-2 font-semibold"
+        >
+          Previous
+        </button>
+        <h4>Page {index}</h4>
+        <button
+          onClick={() => {
+            console.log("next button click");
+            setIndex(index + 1);
+            setUserData([]);
+          }}
+          className="bg-amber-300 text-black rounded cursor-pointer active:scale-95 px-4 py-2 font-semibold"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
